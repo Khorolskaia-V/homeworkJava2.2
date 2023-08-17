20c508315b32d405b13d3d58e65a25a306315244
Виктория <viksyaavito@gmail.com>
2023-08-17 23:29:17 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         int initialBalance = 100;
-        int replenishment = 1100;
+        int replenishment = 1500;
 
-    int bonus = 0;
-    if (replenishment > 1000) {
-        bonus = replenishment / 100;
-    }
+        int bonus = 0;
+        if (replenishment > 1000) bonus = replenishment / 100;
 
-    
+        int finalBalance = initialBalance + replenishment + bonus;
+        System.out.println("Итоговый баланс: " + finalBalance);
+        System.out.println("Бонусы : " + bonus);
     }
 }
