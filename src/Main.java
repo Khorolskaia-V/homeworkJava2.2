b62dfa08e97824e20a3198bc476d85bde446a824
Виктория <viksyaavito@gmail.com>
2023-08-17 23:17:40 +0300

    Replenishment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-    int initialBalance = 100;
-    int re
-        }
+        int initialBalance = 100;
+        int replenishment = 1100;
+
+    int bonus = 0;
+    if (replenishment > 1000) {
+        bonus = replenishment / 100;
     }
+
+    
+    }
+}
